87a6b1703a978a0c7305e9442b28281e66e4c080
Marco Florian Aigner <m.f.aigner@googlemail.com>
2021-11-05 06:03:52 +0100

    Promise.all + map

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const googleScraper = require("google-play-scraper");
 const appleScraper = require("app-store-scraper");
 const converter = require("json-2-csv");
 const fs = require("fs");
+const search = require("app-store-scraper/lib/search");
 const prompt = require("prompt-sync")({ sigint: true });
 
 // Apps stores are represented with an app-store-title and an array of apps, found for given search-terms
@@ -20,16 +21,6 @@ let appleAppStore = {
 };
 
 let apps = [];
-
-// Prompt for search-strings and get rid of eventual (trailing) commas and spaces
-let searchTerms = prompt(
-  "👋Hi! Please enter the terms that you want to scrape for, separated by commas:"
-)
-  .split(/\s*,\s*/)
-  .filter((searchTerm) => searchTerm !== "");
-console.log(`Input: ${searchTerms}`);
-
-//const searchTerms = ['Corona', 'Corona App', 'Corona Warning App', 'Covid-19', 'Covid-19 App', 'Covid-19 App english', 'Contact data', 'Contact details', 'Contact tracing', 'Center for Disease control', 'SARS-CoV-2' ];
 
 // Requires an app-store, then scrapes set store usingn the input search-terms
 // Finally writes the apps found to a .csv-file
@@ -87,7 +78,72 @@ async function scrapeStore(appStore) {
   });
 }
 
+
 // TODO: Aggregate Google and Apple data
 
-scrapeStore(googlePlayStore);
-scrapeStore(appleAppStore);
+// Prompt for search-strings and get rid of eventual (trailing) commas and spaces
+//const searchTerms = prompt(
+//  "👋Hi! Please enter the terms that you want to scrape for, separated by commas:"
+//)
+//  .split(/\s*,\s*/)
+//  .filter((searchTerm) => searchTerm !== "");
+//console.log(`Input: ${searchTerms}`);
+
+//const searchTerms = ['Corona', 'Contact tracing'];
+const searchTerms = ['Corona', 'Corona App', 'Corona Warning App', 'Covid-19', 'Covid-19 App', 'Covid-19 App english', 'Contact data', 'Contact details', 'Contact tracing', 'Center for Disease control', 'SARS-CoV-2' ];
+
+async function scrape (appStore, searchTerm) {
+  let apps = await appStore.scraper.search({
+    term: searchTerm,
+    num: 250
+  })
+
+  apps.forEach(app => app.searchTerm = searchTerm);
+
+  console.log(`Array length for searchTerm ${searchTerm} searching the ${appStore.appStoreTitle}: ${apps.length}`);
+
+  return apps;
+}
+
+async function saveToCsv (iterable, fileName) {
+  const csvData = await converter.json2csvAsync(iterable, {emptyFieldValue: ''});
+
+  fs.writeFile(`scrapedData/${fileName}.csv`, csvData, (err) => {
+    if (err) throw err;
+    console.log(`✍🏽 wrote data to ./scrapedData/${fileName}.csv`);
+  } );
+}
+
+async function saveToJson (iterable, fileName) {
+  const jsonData = JSON.stringify(iterable);
+  fs.writeFile(`scrapedData/${fileName}.json`, jsonData, (err) => {
+    if (err) throw err;
+    console.log(`✍🏽 wrote data to ./scrapedData/${fileName}.json`);
+  } );
+
+}
+
+//scrape(googlePlayStore, 'Contact tracing');
+
+
+(async () => {
+  const googleApps = await Promise.all(searchTerms.map(searchTerm => scrape(googlePlayStore, searchTerm)));
+  const appleApps = await Promise.all(searchTerms.map(searchTerm => scrape(appleAppStore, searchTerm)));
+
+  //saveToCsv(googleApps, 'GoogleApps');
+  //saveToCsv(appleApps, 'Apple Apps');
+
+  //saveToJson(googleApps, 'Google Apps');
+
+  let allApps = [].concat.apply([], [googleApps, appleApps]);
+  allApps = allApps.flat();
+
+  //console.log(JSON.stringify(googleApps, null, 2));
+
+  saveToCsv(allApps, 'Apps');
+
+  
+})()
+
+//scrapeStore(googlePlayStore);
+//scrapeStore(appleAppStore);
