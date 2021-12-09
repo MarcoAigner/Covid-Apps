1cfb6e2be3fb0f62026c4f052f12d44520d20702
Marco Florian Aigner <m.f.aigner@googlemail.com>
2021-12-09 13:01:54 +0100

    Remove JS-files

--- index.ts
+++ index.ts
@@ -2,55 +2,12 @@ import * as googleScraper from "google-play-scraper";
 import * as appleScraper from "app-store-scraper";
 import * as converter from "json-2-csv";
 import * as fs from "fs";
-import { AppStore, GoogleApp, AppleApp } from "./interfaces";
-import * as prompt from "prompt-sync";
+import { AppStore, GoogleApp, AppleApp, CombinedApp } from "./interfaces";
 
 // FIXME: Somehow, each run returns a different amount of apps
 // Check, if the cause is within the script
 
-class CombinedApp {
-  titleGoogle?: string = undefined;
-  titleApple?: string = undefined;
-  urlGoogle?: string = undefined;
-  urlApple?: string = undefined;
-  enUs?: boolean = undefined;
-  covidRelation?: boolean = undefined;
-  additionalProductsRequired?: boolean = undefined;
-  regional?: boolean = undefined;
-  state?: boolean = undefined;
-  pilot?: boolean = undefined;
-  organisational?: boolean = undefined;
-  limitedLiability?: boolean = undefined;
-  comment?: string = undefined;
-  appIdGoogle?: string = undefined;
-  appIdApple?: string = undefined;
-  developerGoogle?: string = undefined;
-  developerApple?: string = undefined;
-  searchTermsGoogle?: Array<string> = undefined;
-  searchTermsApple?: Array<string> = undefined;
 
-  addGoogleAppData(googleApp: GoogleApp) {
-    if (this.titleGoogle) this.searchTermsGoogle?.push(googleApp.searchTerm);
-    else {
-      this.titleGoogle = googleApp.title;
-      this.appIdGoogle = googleApp.appId;
-      this.developerGoogle = googleApp.developer;
-      this.urlGoogle = googleApp.url;
-      this.searchTermsGoogle = [googleApp.searchTerm];
-    }
-  }
-
-  addAppleAppData(appleApp: AppleApp) {
-    if (this.titleApple) this.searchTermsApple?.push(appleApp.searchTerm);
-    else {
-      this.titleApple = appleApp.title;
-      this.appIdApple = appleApp.appId;
-      this.developerApple = appleApp.developer;
-      this.urlApple = appleApp.url;
-      this.searchTermsApple = [appleApp.searchTerm];
-    }
-  }
-}
 
 // Apps stores are represented with an app-store-title and an array of apps, found for given search-terms
 let googlePlayStore: AppStore = {
--- interfaces.ts
+++ interfaces.ts
@@ -1,6 +1,50 @@
 import * as googleScraper from "google-play-scraper";
 import * as appleScraper from "app-store-scraper"
 
+export class CombinedApp {
+  titleGoogle?: string = undefined;
+  titleApple?: string = undefined;
+  urlGoogle?: string = undefined;
+  urlApple?: string = undefined;
+  enUs?: boolean = undefined;
+  covidRelation?: boolean = undefined;
+  additionalProductsRequired?: boolean = undefined;
+  regional?: boolean = undefined;
+  state?: boolean = undefined;
+  pilot?: boolean = undefined;
+  organisational?: boolean = undefined;
+  limitedLiability?: boolean = undefined;
+  comment?: string = undefined;
+  appIdGoogle?: string = undefined;
+  appIdApple?: string = undefined;
+  developerGoogle?: string = undefined;
+  developerApple?: string = undefined;
+  searchTermsGoogle?: Array<string> = undefined;
+  searchTermsApple?: Array<string> = undefined;
+
+  addGoogleAppData(googleApp: GoogleApp) {
+    if (this.titleGoogle) this.searchTermsGoogle?.push(googleApp.searchTerm);
+    else {
+      this.titleGoogle = googleApp.title;
+      this.appIdGoogle = googleApp.appId;
+      this.developerGoogle = googleApp.developer;
+      this.urlGoogle = googleApp.url;
+      this.searchTermsGoogle = [googleApp.searchTerm];
+    }
+  }
+
+  addAppleAppData(appleApp: AppleApp) {
+    if (this.titleApple) this.searchTermsApple?.push(appleApp.searchTerm);
+    else {
+      this.titleApple = appleApp.title;
+      this.appIdApple = appleApp.appId;
+      this.developerApple = appleApp.developer;
+      this.urlApple = appleApp.url;
+      this.searchTermsApple = [appleApp.searchTerm];
+    }
+  }
+}
+
 export interface AppStore {
   title: string,
   short: string,
